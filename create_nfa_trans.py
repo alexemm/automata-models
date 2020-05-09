7ba4318cef72c6d9ac0932d26d31cd850f540b26
Alexander Emmerich <al_emmerich@gmx.de>
2020-05-09 22:52:32 +0200

    Added nfa trans creator

---
+++ create_nfa_trans.py
@@ -0,0 +1,33 @@
+from typing import List, Iterator
+
+
+def get_self_loops(i: int) -> List[str]:
+    # Get all the single byte chars
+    return ["%i %s %i" % (i, chr(lo), i) for lo in range(256) if chr(i) != '\n']
+
+
+def get_all_self_loops(n: int = 13) -> List[str]:
+    return [i for j in range(n) for i in get_self_loops(j)]
+
+
+def get_all_comma_trans(n: int = 13) -> List[str]:
+    return ["%i , %i" % (i, i + 1) for i in range(n - 1)]
+
+
+def get_regex_trans(n: int = 13) -> List[str]:
+    return get_all_comma_trans(n) + get_all_self_loops(n) + ["%i z %i" % (n - 1, n)]
+
+
+def write_into_file(file: str, data: Iterator[str]) -> None:
+    with open(file, 'w+') as f:
+        for item in data:
+            f.write("%s\n" % item)
+
+
+if __name__ == '__main__':
+    n = 13
+    file = "2020_H12.trans"
+    # First rule is for implementation from last week, second rule is for word epsilon
+    regex_trans: Iterator[str] = filter(lambda x: len(x.split()) == 3 and x.split()[1] != '',
+                                        get_regex_trans(n))
+    write_into_file(file, regex_trans)
